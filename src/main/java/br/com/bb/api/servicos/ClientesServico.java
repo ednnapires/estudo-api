e9305765e9b3a30f029f4254e88e2b7d469801e5
f2690752 <f2690752@esbsa069907ka03.df.intrabb.bb.com.br>
2018-01-17 14:36:56 -0200

    Corrigindo identacao

--- src/main/java/br/com/bb/api/servicos/ClientesServico.java
+++ src/main/java/br/com/bb/api/servicos/ClientesServico.java
@@ -1,5 +1,5 @@
 package br.com.bb.api.servicos;
- 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,53 +17,53 @@ import javax.ws.rs.core.Response;
 
 import br.com.bb.api.dao.ClientesDAO;
 import br.com.bb.api.objetos.Clientes;
- 
+
 @Path("/clientes")
 public class ClientesServico {
- 
-    // URI:
-    // /contextPath/servletPath/c
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public List<Clientes> getCliente_JSON() {
-        List<Clientes> listClientes = ClientesDAO.getAllClientes();
-        return listClientes;
-    }
- 
-    // URI:
-    // /contextPath/servletPath/employees/{empNo}
-    @GET
-    @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Clientes getEmployee(@PathParam("id") String id) {
-        return ClientesDAO.getCliente(id);
-    }
- 
-    // URI:
-    // /contextPath/servletPath/employees
-    @POST 
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Clientes addCliente(Clientes cliente) {
-        return ClientesDAO.addCliente(cliente);
-    }
- 
-    // URI:
-    // /contextPath/servletPath/employees
-    @PUT
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Clientes updateCliente(Clientes cliente) {
-        return ClientesDAO.updateCliente(cliente);
-    }
- 
-    @DELETE
-    @Path("/{clienteid}")
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public Response deleteEmployee(@PathParam("clienteid") String clienteid) {
-        ClientesDAO.deleteCliente(clienteid);
-        Map<String, String> teste = new HashMap<>();
-        teste.put("mensagem", "Cliente Deletado  " + clienteid);
-        return Response.ok(teste).build();
-    }
- 
+
+	// URI:
+	// /contextPath/servletPath/c
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public List<Clientes> getCliente_JSON() {
+		List<Clientes> listClientes = ClientesDAO.getAllClientes();
+		return listClientes;
+	}
+
+	// URI:
+	// /contextPath/servletPath/employees/{empNo}
+	@GET
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Clientes getEmployee(@PathParam("id") String id) {
+		return ClientesDAO.getCliente(id);
+	}
+
+	// URI:
+	// /contextPath/servletPath/employees
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Clientes addCliente(Clientes cliente) {
+		return ClientesDAO.addCliente(cliente);
+	}
+
+	// URI:
+	// /contextPath/servletPath/employees
+	@PUT
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Clientes updateCliente(Clientes cliente) {
+		return ClientesDAO.updateCliente(cliente);
+	}
+
+	@DELETE
+	@Path("/{clienteid}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response deleteEmployee(@PathParam("clienteid") String clienteid) {
+		ClientesDAO.deleteCliente(clienteid);
+		Map<String, String> teste = new HashMap<>();
+		teste.put("mensagem", "Cliente Deletado  " + clienteid);
+		return Response.ok(teste).build();
+	}
+
 }
